f0dedefc10604e338b3cef42df0b9664fdead155
Nitai Seri <Nitai.ns@gmail.com>
2022-11-03 13:46:11 +0200

    Basic Skelton hangman

---
+++ React/hangman/src/components/Letter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Letter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Letter;
---
+++ React/hangman/src/components/Letters.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Letters extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Letters;
